8f0e9f319d8fe581cc5d9e9b8bda7065fe9daad5
stepin104973 <adheepgeorgepaul@DESKTOP-HBFNJG5.localdomain>
2020-10-31 09:05:03 +0530

    164.Adding Editing Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
@@ -12,7 +13,9 @@ const appRoutes: Routes = [
     {path: '', redirectTo: '/recipes', pathMatch: 'full'},
     {path:'recipes', component: RecipesComponent, children: [
         {path:'', component: RecipeStartComponent },
-        {path:':id', component: RecipeDetailComponent }
+        {path:'new', component: RecipeEditComponent },
+        {path:':id', component: RecipeDetailComponent },
+        {path:':id/edit', component: RecipeEditComponent }
     ]},
     {path:'shopping-list', component: ShoppingListComponent}
 ]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,8 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective} from './shared/dropdown.directive';
 import { ShoppingListService} from './shopping-list/shopping-list.service';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component'
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component'
 
 
 @NgModule({
@@ -27,7 +28,8 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
-    RecipeStartComponent
+    RecipeStartComponent,
+    RecipeEditComponent
   ],
   imports: [
     BrowserModule,
