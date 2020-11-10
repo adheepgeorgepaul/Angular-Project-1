f7cd3c2abf4b891f4c27a96bcd94531e5feef0cc
adheepgeorgepaul <adheepgeorgepaul@gmail.com>
2020-11-10 20:46:58 +0530

    293.Sending the Signup Request

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
     selector: 'app-auth',
@@ -7,6 +8,7 @@ import { NgForm } from '@angular/forms';
 })
 export class AuthComponent
 {
+    constructor(private authService: AuthService){}
     isLoginMode = true;
 
     onSwitchMode()
@@ -16,7 +18,25 @@ export class AuthComponent
 
     onSubmit(form: NgForm)
     {
-        console.log(form.value);
+        if(!form.valid)
+        {
+            return;
+        }
+
+        const email = form.value.email;
+        const password = form.value.password;
+
+        if(this.isLoginMode)
+        {
+            //..
+        }
+        else
+        {
+            this.authService.signup(email, password)
+                .subscribe((responseData) => {
+                    console.log(responseData);
+                });
+        }
         form.reset();
     }
 }
