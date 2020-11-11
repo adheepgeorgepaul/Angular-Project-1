6ea5fb93f4d5a18e3ad76486ed6eeae6c5b122c6
adheepgeorgepaul <adheepgeorgepaul@gmail.com>
2020-11-11 19:37:31 +0530

    303.Adding Auto-Login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -95,7 +95,36 @@ export class AuthService
         ); 
         
         this.user.next(user);
+        localStorage.setItem('userData', JSON.stringify(user));
     }
+    
+    autologin()
+    {
+        const userData: {
+            email: string,
+            id: string,
+            _token: string,
+            _tokenExpirationDate: string
+        } = JSON.parse(localStorage.getItem('userData'));
+        
+        if(!userData)
+        {
+            return;
+        }
+        
+        const loadedUser = new User(
+            userData.email,
+            userData.id,
+            userData._token,
+            new Date(userData._tokenExpirationDate) 
+        );
+
+        if(loadedUser.token)
+        {
+            this.user.next(loadedUser);
+        } 
+    }
+
 
     logout()
     {
