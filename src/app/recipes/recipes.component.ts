d51947ddd6780dec96c63d86af78f38fb7f04254
adheepgeorgepaul <adheepgeorgepaul@gmail.com>
2020-11-13 12:20:59 +0530

    304.Adding an Auth guard

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -11,6 +11,7 @@ export class RecipesComponent implements OnInit {
   constructor(private dataStorageService: DataStorageService) { }
 
   ngOnInit(){
+    // this.dataStorageService.fetchRecipes().subscribe();
   }   
 
 }
