91d3fc7d2dd0fa3e661cc7b7e437436b0824edf1
adheepgeorgepaul <adheepgeorgepaul@gmail.com>
2020-11-17 09:45:33 +0530

    Deploying an Angular App-Section23-Firebase hosting

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { BehaviorSubject, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 import { User } from './user.model';
+import { environment } from '../../environments/environment';
 
 export interface AuthResponseData
 {
@@ -32,7 +33,7 @@ export class AuthService
     signup(email: string, password: string)
     {   
         return  this.http
-                    .post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCsPo8BHZWeZSm6AWMcqFa2zB1r_QgFRKg',
+                    .post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseAPIKey,
                             {
                                 email: email,
                                 password: password,
@@ -48,7 +49,7 @@ export class AuthService
     login(email: string, password: string)
     {
         return  this.http
-                    .post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCsPo8BHZWeZSm6AWMcqFa2zB1r_QgFRKg',
+                    .post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.firebaseAPIKey,
                         {
                             email: email,
                             password: password,
